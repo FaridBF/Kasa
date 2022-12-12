2de95f65ce23118961ed4d4b7b42bcc897771333
FaridBF <bouras.farid@outlook.fr>
2022-12-12 18:22:34 +0100

    feat: install router on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import About from "./pages/About/About";
+import AccommodationSheet from "./pages/AccommodationSheet/AccommodationSheet";
+import Home from "./pages/Home/Home";
+import Page404 from "./pages/Page404/Page404";
+
 import "./styles/app.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="*" element={<Home />} /> */}</Routes>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accommodationSheet" element={<AccommodationSheet />} />
+        <Route path="/page404" element={<Page404 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
