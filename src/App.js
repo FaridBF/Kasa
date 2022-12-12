8d17ceb1617a261526cd94b87d0f4c42a3ee14c8
FaridBF <bouras.farid@outlook.fr>
2022-12-12 17:41:19 +0100

    build: install dependencies react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./styles/app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>{/* <Route path="*" element={<Home />} /> */}</Routes>
+    </BrowserRouter>
   );
 }
 
