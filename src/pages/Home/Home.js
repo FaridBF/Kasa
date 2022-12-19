647732d4be653cec466547a27664148641bf2ac3
Farid <bouras.farid@outlook.fr>
2022-12-19 11:54:55 +0100

    feat: add page home (banner +header)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,18 @@
-import React from "react";
+import Header from "../../components/Header/Header";
+
+import banner from "../../assets/images/banner.png";
+
 import "../Home/home.css";
 
 function Home(props) {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <>
+      <Header />
+      <div className="banner-container">
+        <img className="banner-img" src={banner} alt="bannière" />
+        <h1 className="title-banner">Chez vous, partout et ailleurs</h1>
+      </div>
+    </>
   );
 }
 
