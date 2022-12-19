499c16c152800e9c19b4aa16890c5d151c9b590e
Farid <bouras.farid@outlook.fr>
2022-12-19 16:17:29 +0100

    feat: add card component in home pages

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,27 @@
 import "./card.css";
 
+import data from "../../data/kasa.json";
+
 function Card() {
-  return <div></div>;
+  return (
+    <>
+      {data.map((element, id) => {
+        console.log("element", element);
+        return (
+          <div key={id} className="card">
+            <img
+              className="cover-card"
+              src={element.cover}
+              alt={element.cover}
+            />
+            <div className="title-container-card">
+              <h1 className="title-card">{element.title}</h1>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Card;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card.js";
 
 import "../Home/home.css";
 
-function Home(props) {
+function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <section className="section">
+        <Card />
+      </section>
     </>
   );
 }
