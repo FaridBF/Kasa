615bdcc2f27a1383c4534280169129bdf1a85584
fbouras-nova <farid.bouras@nova-technology.fr>
2022-12-23 16:59:31 +0100

    feat: add section bottom in AccommodationSheet pages

--- src/pages/AccommodationSheet/AccommodationSheet.js
+++ src/pages/AccommodationSheet/AccommodationSheet.js
@@ -1,15 +1,20 @@
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import DescriptionList from '../../components/DescriptionList/DescriptionList';
+import EquipmentList from '../../components/EquipmentList/EquipmentList';
+import displayStars from '../../utils/displayStars';
 
-import data from "../../data/kasa.json";
+import data from '../../data/kasa.json';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../AccommodationSheet/accommodationSheet.css';
 
-import { useParams } from "react-router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { useParams } from 'react-router';
 
 function AccommodationSheet() {
   let { uid } = useParams();
-  console.log("params", uid);
+  console.log('params', uid);
 
   const accommodation = data.filter((item) => item.id === uid)[0];
   console.log(accommodation);
@@ -17,9 +22,50 @@ function AccommodationSheet() {
   return (
     <>
       <Header />
-      <div>
-        <p>AccommodationSheet</p>
-        <FontAwesomeIcon icon="fa-solid fa-star" />
+
+      <div id='carrousel'>
+        <div id='container'>
+          <FontAwesomeIcon
+            id='g'
+            className='bouton'
+            icon='fa-solid fa-chevron-left'
+          />
+          <FontAwesomeIcon
+            id='d'
+            className='bouton'
+            icon='fa-solid fa-chevron-right'
+          />
+        </div>
+      </div>
+      <div className='section-container'>
+        <div className='description-profil-section'>
+          <h1 className='title-profil-section'>{accommodation.title}</h1>
+          <p className='location-profil-section'>{accommodation.location}</p>
+          {accommodation.tags.map((tag, index) => {
+            return (
+              <span className='tag-profil-section' key={index}>
+                {tag}
+              </span>
+            );
+          })}
+        </div>
+        <div className='profil-section'>
+          <div className='profil-top-section'>
+            <h2>{accommodation.host.name}</h2>
+            <img
+              className='photo-profil'
+              src={accommodation.host.picture}
+              alt='illustration de profil'
+            />
+          </div>
+          <div className='profil-bottom-section'>
+            <span>{displayStars(accommodation.rating)}</span>
+          </div>
+        </div>
+      </div>
+      <div className='descriptionAndEquipement-section'>
+        <DescriptionList accommodation={accommodation} />
+        <EquipmentList accommodation={accommodation} />
       </div>
       <Footer />
     </>
