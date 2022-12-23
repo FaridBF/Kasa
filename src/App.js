db28429460d633da6ee95407ff6e5399104936ae
fbouras-nova <farid.bouras@nova-technology.fr>
2022-12-23 16:58:12 +0100

    feat: add fontawesome

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import About from "./pages/About/About";
-import AccommodationSheet from "./pages/AccommodationSheet/AccommodationSheet";
-import Home from "./pages/Home/Home";
-import Page404 from "./pages/Page404/Page404";
+import About from './pages/About/About';
+import AccommodationSheet from './pages/AccommodationSheet/AccommodationSheet';
+import Home from './pages/Home/Home';
+import Page404 from './pages/Page404/Page404';
 
-import "./styles/app.css";
+import './styles/app.css';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronDown,
+  faChevronRight,
+  faChevronLeft
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCopyright, faStar, faChevronDown);
+library.add(faCopyright, faStar, faChevronDown, faChevronRight, faChevronLeft);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='*' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route
-          path="/accommodationSheet/:uid"
+          path='/accommodationSheet/:uid'
           element={<AccommodationSheet />}
         />
-        <Route path="/404" element={<Page404 />} />
+        <Route path='/404' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
