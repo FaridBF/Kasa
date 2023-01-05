2fdc450771e1a49b26b9165672deadeceaa151f1
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-05 15:23:51 +0100

    fix: correction component DescriptionList and Toogle List (one component for two pages)

--- src/components/DescriptionList/DescriptionList.js
+++ src/components/DescriptionList/DescriptionList.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './descriptionList.css';
 
 function DescriptionList({ data, title }) {
-  // console.log('data', data);
+  const location = useLocation();
+  const currentURLPathname = location.pathname;
+
   const [newdata, setnewData] = useState(true);
   const [isDataList, setIsDataList] = useState(false);
+  const [reliability, setReliability] = useState(true);
+  const [respect, setRespect] = useState(true);
+  const [service, setService] = useState(true);
+  const [security, setSecurity] = useState(true);
 
   useEffect(() => {
     if (Array.isArray(data)) {
@@ -18,32 +26,116 @@ function DescriptionList({ data, title }) {
 
   return (
     <>
-      <section className='container-description-sections'>
-        <div className='description-description-section'>
-          <div onClick={() => setnewData(!newdata)}>
-            <h1 className='title-description-section'>
-              {title}
-              <span className='logo-description-section'>
-                <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
-              </span>
-            </h1>
-          </div>
-          {newdata && isDataList && (
-            <div className='container-description-section'>
-              <div className='position-description-section'>
-                {data.map((equipment, index) => {
-                  return <p key={index}>{equipment}</p>;
-                })}
+      {currentURLPathname === '/about' ? (
+        <>
+          <section className='container-sections'>
+            <div className='reliability-section'>
+              <div onClick={() => setReliability(!reliability)}>
+                <h1 className='title-section'>
+                  Fiabilité
+                  <span className='logo-section'>
+                    <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
+                  </span>
+                </h1>
               </div>
+              {reliability ? (
+                <div className='description-section'>
+                  <p>
+                    Les annonces postées sur Kasa garantissent une fiabilité
+                    totale. Les photos sont conformes aux logements, et toutes
+                    les informations sont régulièrement vérifiées par nos
+                    équipes.
+                  </p>
+                </div>
+              ) : null}
             </div>
-          )}
-          {newdata && !isDataList && (
-            <div className='container-description-section'>
-              <p className='position-description-section'>{data}</p>
+            <div className='respect-section'>
+              <div onClick={() => setRespect(!respect)}>
+                <h1 className='title-section'>
+                  Respect
+                  <span className='logo-section'>
+                    <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
+                  </span>
+                </h1>
+              </div>
+              {respect ? (
+                <p className='description-section'>
+                  La bienveillance fait partie des valeurs fondatrices de Kasa.
+                  Tout comportement discriminatoire ou de perturbation du
+                  voisinage entraînera une exclusion de notre plateforme.
+                </p>
+              ) : null}
             </div>
-          )}
-        </div>
-      </section>
+            <div className='service-section'>
+              <div onClick={() => setService(!service)}>
+                <h1 className='title-section'>
+                  Service
+                  <span className='logo-section'>
+                    <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
+                  </span>
+                </h1>
+              </div>
+              {service ? (
+                <p className='description-section'>
+                  Nos équipes se tiennent à votre disposition pour vous fournir
+                  une expérience parfaite. N'hésitez pas à nous contacter si
+                  vous avez la moindre question.
+                </p>
+              ) : null}
+            </div>
+            <div className='security-section'>
+              <div onClick={() => setSecurity(!security)}>
+                <h1 className='title-section'>
+                  Sécurité
+                  <span className='logo-section'>
+                    <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
+                  </span>
+                </h1>
+              </div>
+              {security ? (
+                <p className='description-section'>
+                  La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes
+                  que pour les voyageurs, chaque logement correspond aux
+                  critères de sécurité établis par nos services. En laissant une
+                  note aussi bien à l'hôte qu'au locataire, cela permet à nos
+                  équipes de vérifier que les standards sont bien respectés.
+                  Nous organisons également des ateliers sur la sécurité
+                  domestique pour nos hôtes.
+                </p>
+              ) : null}
+            </div>
+          </section>
+        </>
+      ) : (
+        <>
+          <section className='container-description-sections'>
+            <div className='description-description-section'>
+              <div onClick={() => setnewData(!newdata)}>
+                <h1 className='title-description-section'>
+                  {title}
+                  <span className='logo-description-section'>
+                    <FontAwesomeIcon icon='fa-solid fa-chevron-down' />
+                  </span>
+                </h1>
+              </div>
+              {newdata && isDataList && (
+                <div className='container-description-section'>
+                  <div className='position-description-section'>
+                    {data.map((equipment, index) => {
+                      return <p key={index}>{equipment}</p>;
+                    })}
+                  </div>
+                </div>
+              )}
+              {newdata && !isDataList && (
+                <div className='container-description-section'>
+                  <p className='position-description-section'>{data}</p>
+                </div>
+              )}
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 }
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,16 +1,14 @@
-import Header from "../../components/Header/Header";
-import Banner from "../../components/Banner/Banner";
-import Footer from "../../components/Footer/Footer";
-import ToogleList from "../../components/ToogleList/ToogleList";
+import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
+import Footer from '../../components/Footer/Footer';
+import DescriptionList from '../../components/DescriptionList/DescriptionList';
 
 function About() {
   return (
     <>
       <Header />
       <Banner />
-      <div>
-        <ToogleList />
-      </div>
+      <DescriptionList />
       <Footer />
     </>
   );
