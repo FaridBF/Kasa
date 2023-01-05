b1f5ae600b4c71418a10b21c21620114bbb434d6
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-05 16:10:29 +0100

    feat: add page 404 in routeur and delete folde utils dans file displayStars

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route
           path='/accommodationSheet/:uid'
           element={<AccommodationSheet />}
         />
-        <Route path='/404' element={<Page404 />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
