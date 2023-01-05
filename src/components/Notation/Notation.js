5657529711a5500f23f59ca32bc5b120b8b3a1bd
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-05 14:16:32 +0100

    fix: correction component notation (displayStars)

---
+++ src/components/Notation/Notation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './notation.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const notation = (num) => {
+  const tab = [];
+  for (let i = 0; i < 5; i++) {
+    if (i < num) {
+      tab.push(
+        <FontAwesomeIcon icon='fa-solid fa-star' key={i} color='#FF6060' />
+      );
+    } else {
+      tab.push(
+        <FontAwesomeIcon icon='fa-solid fa-star' key={i} color='#E3E3E3' />
+      );
+    }
+  }
+  return tab;
+};
+
+export default notation;
