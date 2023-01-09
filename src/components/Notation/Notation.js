423cc90a89fb2878603d8b05a02d54ba18990511
FaridBF <bouras.farid@outlook.fr>
2023-01-09 11:17:20 +0100

    style: modfications px to rem

--- src/components/Notation/Notation.js
+++ src/components/Notation/Notation.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './notation.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const notation = (num) => {
   const tab = [];
   for (let i = 0; i < 5; i++) {
     if (i < num) {
       tab.push(
-        <FontAwesomeIcon icon='fa-solid fa-star' key={i} color='#FF6060' />
+        <FontAwesomeIcon icon="fa-solid fa-star" key={i} color="#FF6060" />
       );
     } else {
       tab.push(
-        <FontAwesomeIcon icon='fa-solid fa-star' key={i} color='#E3E3E3' />
+        <FontAwesomeIcon icon="fa-solid fa-star" key={i} color="#E3E3E3" />
       );
     }
   }
