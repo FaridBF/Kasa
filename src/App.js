e77d51c3d9a41747e41554536538393b12218366
fbouras-nova <farid.bouras@nova-technology.fr>
2023-01-16 15:25:18 +0100

    feat: redirect to 404 page when id is incorrect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import About from "./pages/About/About";
-import AccommodationSheet from "./pages/AccommodationSheet/AccommodationSheet";
-import Home from "./pages/Home/Home";
-import Page404 from "./pages/Page404/Page404";
+import About from './pages/About/About';
+import AccommodationSheet from './pages/AccommodationSheet/AccommodationSheet';
+import Home from './pages/Home/Home';
+import Page404 from './pages/Page404/Page404';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import {
   faChevronDown,
   faChevronRight,
   faChevronLeft
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faCopyright, faStar, faChevronDown, faChevronRight, faChevronLeft);
 
@@ -20,13 +20,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route
-          path="/accommodationSheet/:uid"
+          path='/accommodationSheet/:uid'
           element={<AccommodationSheet />}
         />
-        <Route path="*" element={<Page404 />} />
+        <Route path='*' element={<Page404 />} />
+        <Route path='/error' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/AccommodationSheet/AccommodationSheet.js
+++ src/pages/AccommodationSheet/AccommodationSheet.js
@@ -1,53 +1,58 @@
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import Description from "../../components/Description/Description";
-import notation from "../../components/Notation/Notation";
-import Tag from "../../components/Tag/Tag";
-import Carrousel from "../../components/Carrousel/Carrousel";
+import { Navigate, useParams } from 'react-router';
 
-import data from "../../data/kasa.json";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Description from '../../components/Description/Description';
+import notation from '../../components/Notation/Notation';
+import Tag from '../../components/Tag/Tag';
+import Carrousel from '../../components/Carrousel/Carrousel';
 
-import "../AccommodationSheet/accommodationSheet.css";
+import data from '../../data/kasa.json';
 
-import { useParams } from "react-router";
+import '../AccommodationSheet/accommodationSheet.css';
 
 function AccommodationSheet() {
   let { uid } = useParams();
-  // console.log('params', uid);
-
-  const accommodation = data.filter((item) => item.id === uid)[0];
-  // console.log(accommodation);
+  const accommodation = data.find((item) => item.id === uid);
 
   return (
-    <>
-      <Header />
-      <Carrousel data={accommodation.pictures} />
-      <div className="section-container">
-        <div className="description-profil-section">
-          <h1 className="title-profil-section">{accommodation.title}</h1>
-          <p className="location-profil-section">{accommodation.location}</p>
-          <Tag data={accommodation.tags} />
-        </div>
-        <div className="profil-section">
-          <div className="profil-top-section">
-            <h2>{accommodation.host.name}</h2>
-            <img
-              className="photo-profil"
-              src={accommodation.host.picture}
-              alt="illustration de profil"
-            />
+    <div>
+      {accommodation ? (
+        <>
+          <Header />
+          <Carrousel data={accommodation.pictures} />
+          <div className='section-container'>
+            <div className='description-profil-section'>
+              <h1 className='title-profil-section'>{accommodation.title}</h1>
+              <p className='location-profil-section'>
+                {accommodation.location}
+              </p>
+              <Tag data={accommodation.tags} />
+            </div>
+            <div className='profil-section'>
+              <div className='profil-top-section'>
+                <h2>{accommodation.host.name}</h2>
+                <img
+                  className='photo-profil'
+                  src={accommodation.host.picture}
+                  alt='illustration de profil'
+                />
+              </div>
+              <div className='profil-bottom-section'>
+                <span>{notation(accommodation.rating)}</span>
+              </div>
+            </div>
           </div>
-          <div className="profil-bottom-section">
-            <span>{notation(accommodation.rating)}</span>
+          <div className='descriptionAndEquipement-section'>
+            <Description data={accommodation.description} title='Description' />
+            <Description data={accommodation.equipments} title='Equipements' />
           </div>
-        </div>
-      </div>
-      <div className="descriptionAndEquipement-section">
-        <Description data={accommodation.description} title="Description" />
-        <Description data={accommodation.equipments} title="Equipements" />
-      </div>
-      <Footer />
-    </>
+          <Footer />
+        </>
+      ) : (
+        <Navigate to='/error' />
+      )}
+    </div>
   );
 }
 
